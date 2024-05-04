6586023f48eb8908ccaf0a5ff7c75ce3097662d0
Fai-chebby <chebetf034@gmail.com>
2024-05-04 17:40:57 +0300

    ps

--- password/src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export const Footer=()=>{
-    return(
-        <div className="footer">
-            <div></div>
-            <p>&copy 2024</p>
-        </div>
-    )
-}
